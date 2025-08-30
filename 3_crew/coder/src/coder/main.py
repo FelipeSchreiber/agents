9e368421add366ea0b5f322573716c65e3458580
F3191422 Felipe Schreiber Fernandes <felipe.fernandes@bb.com.br>
2025-08-30 17:16:49 -0300

    fourier transform

--- 3_crew/coder/src/coder/main.py
+++ 3_crew/coder/src/coder/main.py
@@ -11,9 +11,9 @@ warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 # Create output directory if it doesn't exist
 os.makedirs('output', exist_ok=True)
 
-assignment = 'Write a python program to calculate the first 10,000 terms \
-    of this series, multiplying the total by 4: 1 - 1/3 + 1/5 - 1/7 + ...'
-
+assignment = 'Write a python program to calculate the fourier transform of a given signal.\
+    The program should take a list of numerical values as input and return the Fourier transform as a list of complex numbers.\
+        For testing, use the signal of an expoenential decay function defined as f(t) = e^(-t)*cos(2*pi*5*t) for t in the range of 0 to 1 with a step of 0.01.'
 def run():
     """
     Run the crew.
